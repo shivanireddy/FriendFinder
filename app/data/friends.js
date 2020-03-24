d254296f6f55abe28ce70d0fd2df95aa94bda5f6
Shivani Reddy <14326505+shivanireddy@users.noreply.github.com>
2020-03-24 12:11:55 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+// @author: Shivani Sidigidda
+// @github: shivanireddy
+
+
+// Templated Friends Data
+var friendsData = [
+  {
+    name: "Penguin",
+    photo: "https://image.shutterstock.com/image-illustration/vector-illustration-cute-baby-penguin-260nw-747975073.jpg",
+    scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+  },
+  {
+    name: "Superman",
+    photo:"https://i.pinimg.com/originals/28/b4/0f/28b40f2b60b8eece62b46ed96c6bcdee.jpg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    name: "The One",
+    photo:"https://s-media-cache-ak0.pinimg.com/564x/dd/31/d2/dd31d2dd156204fcf9b6aa1c54455adb.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Trinity",
+    photo:"http://vignette3.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest?cb=20060803214449",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Dr. Evil",
+    photo:"https://at-cdn-s01.audiotool.com/2012/05/28/documents/ei368KjdlP5qy6gILDLtH9cEAf6H/0/cover256x256-b31436c2a9bc4645b1ad67bc09705cd7.jpg",
+    scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+  },
+  {
+    name: "Katherine Langford",
+    photo:"https://i.pinimg.com/originals/17/37/31/173731263ac904dbad8792d008d42d74.jpg",
+    scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+  },
+  {
+    name: "Princess Leia",
+    photo:"http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+    scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+  }
+];
+
+
+
+// Export to API Route
+module.exports = friendsData;
